c0cdd444badea496af4816961ba97219a64fbc61
Arini Mauluddina <arini.mauluddina@students.amikom.ac.id>
2022-09-04 17:34:52 +0700

    feat: add audio song

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,56 +36,27 @@
   /**
    * Easy on scroll event listener 
    */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+  // const onscroll = (el, listener) => {
+  //   el.addEventListener('scroll', listener)
+  // }
+
+
 
   /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#navbar .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos,
-      behavior: 'smooth'
-    })
-  }
-
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
+  //  * Back to top button
+  //  */
+  // let backtotop = select('.back-to-top')
+  // if (backtotop) {
+  //   const toggleBacktotop = () => {
+  //     if (window.scrollY > 100) {
+  //       backtotop.classList.add('active')
+  //     } else {
+  //       backtotop.classList.remove('active')
+  //     }
+  //   }
+  //   window.addEventListener('load', toggleBacktotop)
+  //   onscroll(document, toggleBacktotop)
+  // }
 
   /**
    * Mobile nav toggle
@@ -97,35 +68,6 @@
   })
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let body = select('body')
-      if (body.classList.contains('mobile-nav-active')) {
-        body.classList.remove('mobile-nav-active')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
-
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
-  window.addEventListener('load', () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash)
-      }
-    }
-  });
-
-  /**
    * Preloader
    */
   let preloader = select('#preloader');
@@ -134,6 +76,30 @@
       preloader.remove()
     });
   }
+
+  const song = select('#song')
+  on('click', '#btnOpenInvitation', function(e) {
+    song.play()
+  })
+
+  // Play Button
+  let playBtn = select('#btnPlay')
+  on('click', '#btnPlay', function(e) {
+    const classListArray = Array.from(playBtn.classList)
+    let iconPlay = select('#iconPlay')
+
+    if (classListArray.includes('pause')) {
+      playBtn.classList.remove('pause')
+      iconPlay.classList.add('bi-play-fill')
+      iconPlay.classList.remove('bi-pause-fill')
+      song.play()
+    } else {
+      playBtn.classList.add('pause')
+      iconPlay.classList.remove('bi-play-fill')
+      iconPlay.classList.add('bi-pause-fill')
+      song.pause()
+    }
+  })
 
   /**
    * Hero type effect
